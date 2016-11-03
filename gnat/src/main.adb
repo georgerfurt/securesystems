403c60a45e99d171734c889f78d1ad4ead2277d4
Aaron Solbach <Aaron Solbach>
2016-11-03 17:18:20 +0100

    empty project with check-flags added

---
+++ gnat/src/main.adb
@@ -0,0 +1,13 @@
+-- safe and secure systems
+-- assignment 2
+--
+-- by
+-- Georg Erfurt  111653
+-- Aaron Solbach 112201
+
+procedure Main is
+
+begin
+   --  Insert code here.
+   null;
+end Main;
