5f9ac48c068ad143af7d866bbbbbe115c1166d62
Georg Erfurt <georg.erfurt@uni-weimar.de>
2016-11-21 10:30:49 +0100

    election body added

--- gnat/src/elections.adb
+++ gnat/src/elections.adb
@@ -1,4 +1,4 @@
-package Elections is
+package body Elections is
     type Party is (None, A, B, C, D);
     type Votes_Array is array(Party) of Natural;
 
@@ -7,12 +7,45 @@ package Elections is
     Num_Votes_Made: Natural := 0;
     Num_Total_Voters: Natural := 0;
 
-    procedure Initialize(Num_Voters: Natural);
-    -- Resets the number of made votes and votes for all parties to 0, and
-    -- sets the number of total Voters to the given.
-    procedure Vote_For(Vote: Party);
-    function All_Voters_Voted return Boolean;
-    function Find_Winner return Party;
-    -- Returns Party with most votes assigned.
-    -- Returns None if multiple parties share the highest votes.
+   -- Resets the number of made votes and votes for all parties to 0, and
+   -- sets the number of total Voters to the given.
+   procedure Initialize(Num_Voters: Natural) is
+   begin
+      Num_Total_Voters := Num_Voters;
+      Num_Votes_Made := 0;
+      Votes_Distribution := Zero_Votes_Distribution;
+   end;
+
+   procedure Vote_For(Vote: Party) is
+   begin
+      Num_Votes_Made+=1;
+      Votes_Distribution(Vote) := Votes_Distribution(Vote)+1;
+   end;
+
+   function All_Voters_Voted return Boolean is
+   begin
+      return Num_Votes_Made = Num_Total_Voters;
+   end;
+
+   -- Returns Party with most votes assigned.
+   -- Returns None if multiple parties share the highest votes.
+   function Find_Winner return Party is
+   begin
+      Maximum : Element_Type := Votes_Distribution (Votes_Distribution'First);
+      Winner : Party := Votes_Distribution(Votes_Distribution'First);
+
+      for I in Votes_Distribution'First + 1 .. Votes_Distribution'Last loop
+      if Votes_Distribution(I) > Maximum then
+            Maximum := Votes_Distribution (I);
+            Winner := I;
+         elsif Votes_Distribution(I) = Maximum then
+            Winner := none;
+
+      end if;
+      end loop;
+
+      return Winner;
+
+   end;
+
 end Elections;
